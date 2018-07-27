86eeb3ba4f3a72538c210a411ef0a9220c269de2
DKSadx <40239132+DKSadx@users.noreply.github.com>
2018-07-27 10:50:48 +0200

    Add files via upload

---
+++ joynew/joy/joy.ino
@@ -0,0 +1,22 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte addresses[6] = {"00001"};
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(addresses); // 00001
+  radio.setPALevel(RF24_PA_MIN);
+}
+void loop() {
+  delay(5);
+  int X_osa = analogRead(A0);
+  int Y_osa = analogRead(A2);
+ //  X_osa= map(X_osa, 0, 1023, 0, 255);
+ //  Y_osa= map(Y_osa,0,1023,0,255);
+  radio.write(&X_osa, sizeof(X_osa));
+  radio.write(&Y_osa, sizeof(Y_osa));
+  
+    
+  
+}
--- joynew/joynew.ino
+++ joynew/joynew.ino
@@ -1,16 +1,17 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(8, 9); // CE, CSN
 const byte addresses[6] = {"00001"};
 int L1 = 2;
 int R1 = 3;
 int btn1 = 4;
 int btn2 = 5;
 int btn3 = 6;
-int btn4 = 9;
+int btn4 = 7;
+int led = 10;
 void setup() {
-//  Serial.begin(9600);
+// Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(addresses); // 00001
   radio.setPALevel(RF24_PA_MIN);
@@ -20,7 +21,10 @@ void setup() {
   pinMode(btn2,INPUT_PULLUP);
   pinMode(btn3,INPUT_PULLUP);
   pinMode(btn4,INPUT_PULLUP);
+  pinMode(led,OUTPUT);
+  digitalWrite(led,HIGH);
 }
+
 void loop() {
    delay(5);
   int ose[10];
@@ -29,23 +33,23 @@ void loop() {
   ose[2]= analogRead(1);
   ose[3]= analogRead(2);
   
-//  Serial.print("Analog 1:   ");
-//  Serial.print(ose[0]);
-//  Serial.print("   ");
-//  Serial.println(ose[1]);
-//  Serial.print("Analog 2:   ");
-//  Serial.print(ose[2]);
-//  Serial.print("   ");
-//  Serial.println(ose[3]);
+ /* Serial.print("Analog 1:   ");
+  Serial.print(ose[0]);
+  Serial.print("   ");
+  Serial.println(ose[1]);
+  Serial.print("Analog 2:   ");
+  Serial.print(ose[2]);
+  Serial.print("   ");
+  Serial.println(ose[3]);*/
   ose[4]= digitalRead(2);
   ose[5]= digitalRead(3);
   ose[6]= digitalRead(4);
   ose[7]= digitalRead(5);
   ose[8]= digitalRead(6);
   ose[9]= digitalRead(9);
- // for(int i=4;i<=9;i++){
- //   Serial.print("Button "); Serial.print(i-3);Serial.print("  :     "); Serial.println(ose[i]);
- // }
+/*  for(int i=4;i<=9;i++){
+   Serial.print("Button "); Serial.print(i-3);Serial.print("  :     "); Serial.println(ose[i]);
+  }*/
   radio.write(ose, sizeof(ose));
 
   
